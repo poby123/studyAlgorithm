cd8193156f4fda1eddd2332af829db54506e5108
poby123 <youngsm12@naver.com>
2021-02-08 20:53:19 +0900

    N과 M (3)(백트래킹)

---
+++ 백준/15651.cpp
@@ -0,0 +1,53 @@
+/*
+문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 7)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 
+중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> v;
+
+void pick(vector<int> &v, int toPick)
+{
+    if (!toPick)
+    {
+        for (auto i : v)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        v.push_back(i);
+        pick(v, toPick - 1);
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+    v.reserve(N);
+
+    pick(v, M);
+    return 0;
+}
